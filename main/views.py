f5d59a85ae910db4a4ab2a55760d4261351bd0ab
seita-uchida <seita_uchida@waotech.jp>
2025-12-09 20:38:57 +0900

    first commit

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def signup(request):
+    return render(request, 'main/signup.html')
+
+def login(request):
+    return render(request, 'main/login.html')

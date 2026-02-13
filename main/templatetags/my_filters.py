cbb4387cddf463431f463417bbb4005def89022f
seita-uchida <seita_uchida@waotech.jp>
2026-02-13 18:40:44 +0900

    トーク画面の表示完成

--- main/templatetags/my_filters.py
+++ main/templatetags/my_filters.py
@@ -3,5 +3,10 @@ from django import template
 register = template.Library()
 
 @register.filter(name="censor")
-def censor(value, arg):
-    return value.replace(arg, "〇" * len(arg))
+def censor(value, word):
+    """
+    value: 元のメッセージ
+    word: 消したい言葉
+    """
+    # その言葉を「〇」に置き換える（文字数分）
+    return value.replace(word, "〇" * len(word))

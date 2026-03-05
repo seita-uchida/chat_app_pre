98a829f078b5df11a3088d0a8957173c0531f190
seita-uchida <seita_uchida@waotech.jp>
2026-03-05 20:49:12 +0900

    update toの試し

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models    
 
 class User(AbstractUser):
-    icon = models.ImageField(null=True, blank=True)
+    icon = models.ImageField(upload_to='icons/',null=True, blank=True)
 
 class Talk(models.Model):
     # メッセージ
